c4f50d1304b911572ec5f7b5c2a8d05c82d802e8
Stephanie deWet <sdewet@cs.wisc.edu>
2014-05-03 15:05:52 -0500

    Clean up run_optimization script

--- code/run_optimization.m
+++ code/run_optimization.m
@@ -17,42 +17,26 @@ end
 
 % Run the optimizations several times
 D = size(X, 2);
-theta = zeros(D+2,4,4);
-fval = zeros(4,4);
+theta = zeros(D+2,4,3);
+fval = zeros(4,3);
 
 randSet = randperm(N, 100);
 
-% Specially scaled 1-100
-SampleSize = 100;
-local_Scaled_X = zeros(SampleSize, D);
-local_mean_y = mean(y(1:SampleSize));
-local_stdev_y = std(y(1:SampleSize));
-local_mean_X = mean(X(1:SampleSize, :),1);
-local_stdev_X = std(X(1:SampleSize, :),0,1);
-local_Scaled_y = y - local_mean_y;
-for i = 1:N
-    local_Scaled_X(i,:) = (X(i,:) - local_mean_X) ./ local_stdev_X;
-end
-
-for cov_type = 1:1
-    for data_set = 1:1
+for cov_type = 1:4
+    for data_set = 1:3
         switch data_set
             case 1 % Data between 1 and 100
                 set = 1:100;
                 X_set = Scaled_X(set,:);
                 y_set = Scaled_y(set);
-            case 2 % Data between 10001 and 10100
-                set = 10001:10100;
+            case 2 % Data between 8001 and 8100
+                set = 8001:8100;
                 X_set = Scaled_X(set,:);
                 y_set = Scaled_y(set);
             case 3 % Randomly selected data
                 set = randSet;
                 X_set = Scaled_X(set,:);
                 y_set = Scaled_y(set);
-            case 4 % Data between 1 and 100, normalized specifically for this case
-                set = 1:100;
-                X_set = local_Scaled_X(set, :);
-                y_set = local_Scaled_y;
         end
         local_optima = [];
         local_optimizers = [];
@@ -84,6 +68,7 @@ for cov_type = 1:1
         [m, i] = max(local_optima);
         theta(:, cov_type, data_set) = m;
         fval(:, cov_type, data_set) = local_optima(:,i);
+        fprintf('local optimum: %f', m);
     end
 end
 
