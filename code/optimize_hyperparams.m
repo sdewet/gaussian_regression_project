72b5c33f13b72af7daa9c0897de879dc92e464e4
Stephanie deWet <sdewet@cs.wisc.edu>
2014-05-06 14:53:49 -0500

    Revert "Try not recalculating Xi-Xj over and over again"
    This reverts commit 749a37d6b5ab9817dc20a6d24a67cbc6a6821418.

--- code/optimize_hyperparams.m
+++ code/optimize_hyperparams.m
@@ -11,22 +11,9 @@ Xt = X'; % Each column of X is a 27-param table entries
 
 % Lower bound - bound all to be >= 0
 lb = zeros(n_theta,1);
-%ub = 50 * ones(n_theta,1);
+ub = 50 * ones(n_theta,1);
 %lb(n_theta) = 0;
 %ub(n_theta) = 1e-8;
-
-D = size(Xt, 2);
-
-global X_prime;
-X_prime = zeros(D,D,27);
-
-for (i = 1:D)
-	for j = i:D
-		X_prime(i,j,:) = Xt(:,i) - Xt(:,j);
-		X_prime(j,i,:) = X_prime(i,j,:);
-	end
-end
-
 
 switch cov_type
 	case 1  % Squared exponential
@@ -43,7 +30,7 @@ switch cov_type
             'MaxFunEvals',5000, ...
             'MaxIter', 1500, ...
 			'Algorithm','interior-point');  % or trust-region-reflective
-		objective = @(theta) exponential_cov_obj(theta,y);
+		objective = @(theta) exponential_cov_obj(Xt,y,theta);
 	case 3  % Matern (3/2)
 		options = optimoptions(@fmincon,...
 			'Display','iter', ...
